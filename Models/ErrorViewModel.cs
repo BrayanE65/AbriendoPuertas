6f25ba0679b8486aebd250aa3042b4a3fc1e1d7c
brayancorredor <bredcom65@gmail.com>
2022-05-03 19:31:42 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AbriendoPuertas.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
